e996aa3a962f1ca089cda49b2eeb6e07fe9a132d
Gabor Szabo <szabogabi@gmail.com>
2016-04-15 14:28:46 +0200

    ketsoros fejlogo

--- templates/header-mobile.php
+++ templates/header-mobile.php
@@ -1,4 +1,4 @@
-<header class="mobileheader hide-for-tablet">
+<header class="mobileheader hide-for-medium">
   <div class="row container">
     <div class="columns small-6">
       <a class="sitelogo sitelogo--mobil" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,9 @@
 <header class="siteheader">
   <div class="row container">
-    <div class="columns tablet-4">
+    <div class="columns medium-4">
       <a class="sitelogo" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     </div>
-    <div class="columns tablet-8">
+    <div class="columns medium-8">
       <?php if (has_nav_menu('primary_navigation')) : ?>
         <nav class="sitenav">
           <?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu menu--sitemenu']); ?>
