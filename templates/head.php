f5eb79ad5dde45e1dfc6e87447031f1b0bbed3f9
Gabor Szabo <szabogabi@gmail.com>
2016-04-28 16:03:15 +0200

    favicons added

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri();?>/dist/images/favicon/apple-touch-icon.png">
+  <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri();?>/dist/images/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-TileImage" content="<?= get_stylesheet_directory_uri();?>/dist//images/favicon/mstile-144x144.png">
 </head>
