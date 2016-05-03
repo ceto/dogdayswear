eddd8148c00c61b93ced22f8d5154f95f8130b98
Gabor Szabo <szabogabi@gmail.com>
2016-05-03 14:18:21 +0200

    mc added to live

--- templates/mailchimp-subscribe.php
+++ templates/mailchimp-subscribe.php
@@ -9,7 +9,7 @@
         <div class="columns xlarge-5 text-right">
           <!-- Begin MailChimp Signup Form -->
           <div id="mc_embed_signup">
-            <form action="//dogdayswear.us12.list-manage.com/subscribe/post?u=4392a96715883db49eda0d8bd&amp;id=d5d3bd152a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" data-abide novalidate>
+            <form action="//dogdayswear.us12.list-manage.com/subscribe/post-json?u=4392a96715883db49eda0d8bd&amp;id=d5d3bd152a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
               <div class="input-group">
                 <input type="email" value="" placeholder="Add meg az e-mail címedet" name="EMAIL" required class="input-group-field" id="mce-EMAIL">
@@ -17,13 +17,10 @@
                   <input type="submit" value="<?php _e('Jöhet','dd') ?>" name="subscribe" id="mc-embedded-subscribe" class="button">
                 </div>
               </div>
-              <div id="mce-responses" class="clear">
-                <div class="response" id="mce-error-response" style="display:none"></div>
-                <div class="response" id="mce-success-response" style="display:none"></div>
+              <div id="mce-responses" class="mce-responses">
               </div>
           </form>
           </div>
-          <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
           <!--End mc_embed_signup-->
         </div>
       </div>
