9cb159c9b4e2d5c21875f782c91cf59f2bcd24f7
Szabó Gábor <szabogabor@hydrogene.hu>
2017-01-17 12:52:07 +0100

    Leaf color added

--- tmpl-home.php
+++ tmpl-home.php
@@ -8,7 +8,7 @@
     <div class="columns medium-10 medium-centered large-centered large-10 xlarge-8 xxlarge-6">
       <div class="hero__content">
           <h1 class="hero__title">Jóga és sportruházat a legizzasztóbb mozgásformákhoz.</h1>
-          <a href="<?= get_permalink( woocommerce_get_page_id( 'shop' ) )?>" class="hero__more button">Vásárlás</a>
+          <a href="<?= get_permalink( woocommerce_get_page_id( 'shop' ) )?>" class="hero__more button">Shop</a>
       </div>
     </div>
   </div>
