e1b22ffdd7db09f269f94d7924b8ce4962f35072
Szabó Gábor <szabogabor@hydrogene.hu>
2017-04-06 22:08:51 +0200

    .images added back needed by imageswap by attributes

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post, $product;
 ?>
-<div class="productimages">
+<div class="productimages images">
 	<?php
 		if ( has_post_thumbnail() ) {
 			$attachment_count = count( $product->get_gallery_attachment_ids() );
