6be662e2c09b6488626f6eaf882934322914124c
Szabó Gábor <szabogabor@hydrogene.hu>
2017-05-18 16:00:28 +0200

    wc template files upgraded to 3.0.7 in the theme first act

--- woocommerce/emails/email-addresses.php
+++ woocommerce/emails/email-addresses.php
@@ -4,39 +4,33 @@
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-addresses.php.
  *
- * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
- * will need to copy the new files to your theme to maintain compatibility. We try to do this.
- * as little as possible, but it does happen. When this occurs the version of the template file will.
- * be bumped and the readme will list any important changes.
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
  *
- * @see 	    http://docs.woothemes.com/document/template-structure/
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates/Emails
- * @version     2.4.0
+ * @version     3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$text_align = is_rtl() ? 'right' : 'left';
+
 ?><table id="addresses" cellspacing="0" cellpadding="0" style="width: 100%; vertical-align: top;" border="0">
 	<tr>
-		<td class="td" style="text-align:left; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" valign="top" width="50%">
+		<td class="td" style="text-align:<?php echo $text_align; ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" valign="top" width="50%">
 			<h3><?php _e( 'Billing address', 'woocommerce' ); ?></h3>
 
 			<p class="text"><?php echo $order->get_formatted_billing_address(); ?></p>
 		</td>
-		<?php //this php block added by ceto checking local pickup
-			$shipping_local_pickup = false;
-			//if ( $order->has_shipping_method( 'local_pickup' ) ) { $shipping_local_pickup = true; }
-			if ( $items_totals = $order->get_order_item_totals() ) {
-			    foreach ( $items_totals as $items_total ) {
-			        if ( $items_total['value'] == 'Személyes átvétel' && !$shipping_local_pickup ) $shipping_local_pickup = true;
-			    }
-			}
-		?>
-		<?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() && ( $shipping = $order->get_formatted_shipping_address() ) && !$shipping_local_pickup ) : ?>
-			<td class="td" style="text-align:left; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" valign="top" width="50%">
+		<?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() && ( $shipping = $order->get_formatted_shipping_address() ) ) : ?>
+			<td class="td" style="text-align:<?php echo $text_align; ?>; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" valign="top" width="50%">
 				<h3><?php _e( 'Shipping address', 'woocommerce' ); ?></h3>
 
 				<p class="text"><?php echo $shipping; ?></p>
--- woocommerce/myaccount/form-lost-password.php
+++ woocommerce/myaccount/form-lost-password.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.6.0
+ * @version 3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -25,24 +25,24 @@ wc_print_notices(); ?>
 	<div class="columns tablet-10 tablet-centered xlarge-8">
 		<form method="post" class="woocommerce-ResetPassword lost_reset_password">
 
-	<p><?php echo apply_filters( 'woocommerce_lost_password_message', __( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p>
+			<p><?php echo apply_filters( 'woocommerce_lost_password_message', __( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p>
 
-	<p class="woocommerce-FormRow woocommerce-FormRow--first form-row form-row-first">
-		<label for="user_login"><?php _e( 'Username or email', 'woocommerce' ); ?></label>
-		<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" />
-	</p>
+			<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
+				<label for="user_login"><?php _e( 'Username or email', 'woocommerce' ); ?></label>
+				<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" />
+			</p>
 
-	<div class="clear"></div>
+			<div class="clear"></div>
 
-	<?php do_action( 'woocommerce_lostpassword_form' ); ?>
+			<?php do_action( 'woocommerce_lostpassword_form' ); ?>
 
-	<p class="woocommerce-FormRow form-row">
-		<input type="hidden" name="wc_reset_password" value="true" />
-		<input type="submit" class="woocommerce-Button button" value="<?php esc_attr_e( 'Reset Password', 'woocommerce' ); ?>" />
-	</p>
+			<p class="woocommerce-form-row form-row">
+				<input type="hidden" name="wc_reset_password" value="true" />
+				<input type="submit" class="woocommerce-Button button" value="<?php esc_attr_e( 'Reset password', 'woocommerce' ); ?>" />
+			</p>
 
-	<?php wp_nonce_field( 'lost_password' ); ?>
+			<?php wp_nonce_field( 'lost_password' ); ?>
 
-</form>
+		</form>
 	</div>
 </div>
