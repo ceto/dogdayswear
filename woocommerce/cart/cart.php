1b00d9d1ee1c4c7c088f147704b6d539c6050f77
Szabó Gábor <szabogabi@gmail.com>
2017-07-25 12:31:23 +0200

    cart & review_order updated

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -2,13 +2,22 @@
 /**
  * Cart Page
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.3.8
+ * @version 3.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 ?>
 
@@ -38,36 +47,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 					$product_id   = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
 					if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+						$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
 						?>
-						<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+						<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 							<td class="product-name">
 								<span class="product-thumbnail">
 									<?php
-									$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+										$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
 
-									if ( ! $_product->is_visible() ) {
-										echo $thumbnail;
-									} else {
-										printf( '<a href="%s">%s</a>', esc_url( $_product->get_permalink( $cart_item ) ), $thumbnail );
-									}
-								?>
+										if ( ! $product_permalink ) {
+											echo $thumbnail;
+										} else {
+											printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail );
+										}
+									?>
 								</span>
-								<?php
-									if ( ! $_product->is_visible() ) {
-										echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ) . '&nbsp;';
-									} else {
-										echo apply_filters( 'woocommerce_cart_item_name', sprintf( '<a class="prodtitle" href="%s">%s </a>', esc_url( $_product->get_permalink( $cart_item ) ), $_product->get_title() ), $cart_item, $cart_item_key );
-									}
-									// Meta data
-									echo WC()->cart->get_item_data( $cart_item );
-								?>
-								| <span class="product-price">
+								<span class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
+									<?php
+										if ( ! $product_permalink ) {
+											echo apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;';
+										} else {
+											echo apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key );
+										}
+
+										// Meta data
+										echo WC()->cart->get_item_data( $cart_item );
+
+										// Backorder notification
+										if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+											echo '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>';
+										}
+									?>
+								</span>
+								<br>
+								<span class="product-price" data-title="<?php esc_attr_e( 'Price', 'woocommerce' ); ?>">
 									<strong><?php _e( 'Price', 'woocommerce' ); ?>:</strong>&nbsp;<?php
 									echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
 									?>
 								</span>
-<!-- 								<span class="product-quantity">
-								<br>
+<!--  								<span class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
 									<?php
 										if ( $_product->is_sold_individually() ) {
 											$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
@@ -75,8 +93,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 											$product_quantity = woocommerce_quantity_input( array(
 												'input_name'  => "cart[{$cart_item_key}][qty]",
 												'input_value' => $cart_item['quantity'],
-												'max_value'   => $_product->backorders_allowed() ? '' : $_product->get_stock_quantity(),
-												'min_value'   => '0'
+												'max_value'   => $_product->get_max_purchase_quantity(),
+												'min_value'   => '0',
 											), $_product, false );
 										}
 
@@ -92,7 +110,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 								<span class="product-remove">
 									<?php
 										echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
-											'<a href="%s" class="remove" title="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+											'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
 											esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
 											__( 'Remove this item', 'woocommerce' ),
 											esc_attr( $product_id ),
@@ -104,7 +122,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 
-							<td class="product-subtotal">
+							<td class="product-subtotal" data-title="<?php esc_attr_e( 'Total', 'woocommerce' ); ?>">
 								<?php
 									echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key );
 								?>
@@ -116,7 +134,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				do_action( 'woocommerce_cart_contents' );
 				?>
-<!-- 				<tr>
+<!--  				<tr>
 					<td colspan="2" class="actions text-right">
 						<input type="submit" class="button tiny update_cart" name="update_cart" value="<?php esc_attr_e( 'Update Cart', 'woocommerce' ); ?>" />
 						<?php do_action( 'woocommerce_cart_actions' ); ?>
@@ -125,7 +143,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 				</tr> -->
 				<tr>
 					<td colspan="2" class="actions">
-						<?php if ( WC()->cart->coupons_enabled() ) { ?>
+						<?php if ( wc_coupons_enabled() ) { ?>
 							<div class="coupon">
 								<label for="coupon_code"><?php _e( 'Coupon', 'woocommerce' ); ?>:</label>
 								<div class="input-group">
@@ -152,7 +170,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="columns tablet-10 tablet-pull-1 xlarge-5 xlarge-pull-0 xxlarge-4">
 		<div class="cart-collaterals">
-			<?php do_action( 'woocommerce_cart_collaterals' ); ?>
+			<?php
+				/**
+				 * woocommerce_cart_collaterals hook.
+				 *
+				 * @hooked woocommerce_cross_sell_display
+				 * @hooked woocommerce_cart_totals - 10
+				 */
+				do_action( 'woocommerce_cart_collaterals' );
+			?>
 		</div>
 
 		<?php do_action( 'woocommerce_after_cart' ); ?>
