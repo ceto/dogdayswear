a0d87805914ad21e95d4fe43bef44f92dbfe02a7
Szabó Gábor <szabogabi@gmail.com>
2017-07-27 11:07:22 +0200

    pcard thumbnail synced

--- woocommerce/loop/pcard-thumbnails.php
+++ woocommerce/loop/pcard-thumbnails.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Added by Ceto. Not part of core
+ * Added by Ceto. Not part of the wc core
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -9,38 +9,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post, $product, $woocommerce;
 
-$attachment_ids = $product->get_gallery_attachment_ids();
+$attachment_ids = $product->get_gallery_image_ids();
 
 if ( has_post_thumbnail() ) {
  $attachment_ids = array_merge( array(get_post_thumbnail_id()) , $attachment_ids );
 }
 
 
-if ( $attachment_ids ) {
+if ( $attachment_ids && has_post_thumbnail() ) {
 	$loop = 0;
 	?>
-	<div class="catalogthumbs owl-carousel"><?php
+	<div class="catalogthumbs owl-carousel">
+		<?php
 
 		foreach ( $attachment_ids as $attachment_id ) {
+			$attributes = array(
+				'title'                   => get_post_field( 'post_title', $attachment_id ),
+				'data-caption'            => get_post_field( 'post_excerpt', $attachment_id ),
+				'data-src'                => $full_size_image[0],
+				'data-large_image'        => $full_size_image[0],
+				'data-large_image_width'  => $full_size_image[1],
+				'data-large_image_height' => $full_size_image[2],
+			);
 
-			$image_class = implode( ' ', $classes );
-			$props       = wc_get_product_attachment_props( $attachment_id, $post );
+			$html  = '<div class="item"><a href="' . esc_url( get_the_permalink() ) . '" title="'. esc_attr( get_the_title() ) .'">';
+			$html .= wp_get_attachment_image( $attachment_id, 'shop_catalog', false, $attributes );
+ 			$html .= '</a></div>';
 
-			if ( ! $props['url'] ) {
-				continue;
-			}
-
-			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '
-				<div class="item"><a href="%s" class="%s" title="%s">%s</a></div>',
-					esc_url( get_the_permalink() ),
-					esc_attr( $image_class ),
-					esc_attr( get_the_title() ),
-					wp_get_attachment_image( $attachment_id, apply_filters( 'single_product_small_thumbnail_size', 'shop_catalog' ), 0, $props )
-					),
-					$attachment_id,
-					$post->ID,
-					esc_attr( $image_class )
-					);
+			echo $html;
 
 			if ($loop++ == 1) {
 				break;
