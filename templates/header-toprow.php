cd7ad9bd5c52c7e0efa2a0ea8211435c0f54c7d2
Gábor Szabó <szabogabi@gmail.com>
2019-01-17 12:59:17 +0100

    telszam

--- templates/header-toprow.php
+++ templates/header-toprow.php
@@ -1,6 +1,6 @@
 <aside id="toprow" class="toprow">
   <div class="toprow__first">
-    <span class="callnow"><i class="icon icon--envelope"></i> <a href="mailto:hello@dogdayswear.com">hello@dogdayswear.com</a></span> | <span class="callnow"><i class="icon icon--phone2"></i> <a href="tel:0036205954060">(+36) 20.595.4060</a></span>
+    <span class="callnow"><i class="icon icon--envelope"></i> <a href="mailto:hello@dogdayswear.com">hello@dogdayswear.com</a></span> | <span class="callnow"><i class="icon icon--phone2"></i> <a href="tel:+36308598291">(+36) 30.859.8291</a></span>
   </div>
   <div class="toprow__sec">
     <span class="callnow"><a href="#" class="localitoggler" data-toggle="localization-dropdown"><img src="<?= get_stylesheet_directory_uri().'/dist/images/flags/'. dd_get_customercountrycode().'.png'; ?>" alt="<?php echo dd_get_customercountrycode(); ?>"> <?= ICL_LANGUAGE_CODE ?></a></span> |
